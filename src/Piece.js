9e3e15aef7af5bcf3401482cfdee9cbc52ee0840
unknown <yeehawbw@gmail.com>
2020-05-14 13:31:57 -0400

    Add drag preview image

--- src/Piece.js
+++ src/Piece.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { DndItemTypes } from './DndItemTypes';
-import { useDrag } from 'react-dnd';
+import { useDrag, DragPreviewImage } from 'react-dnd';
 
 export default function Piece({ symbol, x, y }) {
-  const [{ isDragging }, drag] = useDrag({
+  const [{ isDragging }, drag, preview] = useDrag({
     item: { type: DndItemTypes.PIECE, symbol: symbol, x: x, y: y },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
@@ -11,8 +11,14 @@ export default function Piece({ symbol, x, y }) {
   });
 
   return (
-    <div ref={drag} className='piece' style={{ opacity: isDragging ? 0.5 : 1 }}>
-      {symbol}
-    </div>
+    <>
+      <DragPreviewImage connect={preview} src={require('./acorn.svg')} />
+      <div
+        ref={drag}
+        className='piece'
+        style={{ opacity: isDragging ? 0.5 : 1 }}>
+        {symbol}
+      </div>
+    </>
   );
 }
