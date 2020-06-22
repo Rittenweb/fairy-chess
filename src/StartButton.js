bdd82339f0f48f9a46f2530e02e097670b06a93f
unknown <yeehawbw@gmail.com>
2020-06-22 15:57:35 -0400

    Add first audio

--- src/StartButton.js
+++ src/StartButton.js
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { DispatchContext } from './Contexts';
+import useSound from 'use-sound';
+import startSound from './audio/enchanted_forest.mp3';
 
 export default function StartButton({ gameOver }) {
   const dispatch = useContext(DispatchContext);
+  const [play] = useSound(startSound, { loop: true, volume: 0.5 });
 
   const start = function start(e) {
+    play();
     dispatch({
       type: 'transitionstart',
     });
