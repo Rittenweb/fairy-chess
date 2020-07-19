7db7f3462d7bb966f22f8904c566ffbe2dd9b58c
unknown <yeehawbw@gmail.com>
2020-07-18 21:26:48 -0400

    First version of board teststate

--- src/App.test.js
+++ src/App.test.js
@@ -15,9 +15,9 @@ import {
   enemyPieceDefs,
   pieceDefs,
 } from './pieceData';
-import initialState from './baseStates';
+import { testState } from './testStates';
 
-it('renders banner', () => {
+it('renders without crashing', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Fairy Chess/i);
   expect(linkElement).toBeInTheDocument();
@@ -40,4 +40,27 @@ it('returns a proper new setup piece', () => {
     id: expect.any(Number),
   };
   expect(result).toEqual(schema);
+});
+
+it('lets enemies move only as far as they can', () => {
+  //A three-square moving enemy
+  //Which is blocked in all three squares should not return a square
+  expect(getEnemyMoveSquare(0, 3, testState.pieces)).toBeUndefined();
+  //Which is blocked for the furthest two squares should return one square down
+  expect(getEnemyMoveSquare(1, 3, testState.pieces)).toEqual([1, 4]);
+  //Which is blocked for the furthest one square should return two down
+  expect(getEnemyMoveSquare(2, 3, testState.pieces)).toEqual([2, 5]);
+  //Which is not blocked should return three down
+  expect(getEnemyMoveSquare(3, 3, testState.pieces)).toEqual([3, 6]);
+});
+
+it('all enemy pieces should a valid move value', () => {
+  for (let piece of Object.keys(enemyPieceDefs)) {
+    let move = getEnemyMoveSquareRelative(piece)[1];
+    expect(move === 1 || move === 2 || move === 3).toBeTruthy();
+  }
+});
+
+it('an enemy that can capture two pieces should take the rarer', () => {
+  expect(getEnemyCapSquare(0, 3, testState.pieces)).toEqual([0, 2]);
 });
---
+++ src/testStates.js
@@ -0,0 +1,681 @@
+export const testState = {
+  gamePhase: 'start', //Can be: start, setup, inprogress, rewards, gameover, transitionstart, transitioninprogress, transitionnoboard
+  enemyCaptureShown: false,
+  shouldTurnEnd: false,
+  benchPieces: [],
+  choicesList: [],
+  wave: 0,
+  volume: .5,
+  pieces: {
+    '0': {
+      '0': null,
+      '1': null,
+      '2': {
+        name: 'queen',
+        enemy: false,
+        exhausted: false
+      },
+      '3': {
+        name: 'forestmother',
+        enemy: true,
+        exhausted: false
+      },
+      '4': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '5': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '6': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '1': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': {
+        name: 'forestmother',
+        enemy: true,
+        exhausted: false
+      },
+      '4': null,
+      '5': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '6': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '2': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': {
+        name: 'forestmother',
+        enemy: true,
+        exhausted: false
+      },
+      '4': null,
+      '5': null,
+      '6': {
+        name: 'bishop',
+        enemy: false,
+        exhausted: false
+      },
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '3': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': {
+        name: 'forestmother',
+        enemy: true,
+        exhausted: false
+      },
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '4': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '5': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '6': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '7': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '8': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '9': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '10': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+    '11': {
+      '0': null,
+      '1': null,
+      '2': null,
+      '3': null,
+      '4': null,
+      '5': null,
+      '6': null,
+      '7': null,
+      '8': null,
+      '9': null,
+      '10': null,
+      '11': null,
+    },
+  },
+  squares: {
+    '0': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '1': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '2': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '3': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '4': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '5': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '6': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '7': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '8': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '9': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '10': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+    '11': {
+      '0': {
+        captureMark: false
+      },
+      '1': {
+        captureMark: false
+      },
+      '2': {
+        captureMark: false
+      },
+      '3': {
+        captureMark: false
+      },
+      '4': {
+        captureMark: false
+      },
+      '5': {
+        captureMark: false
+      },
+      '6': {
+        captureMark: false
+      },
+      '7': {
+        captureMark: false
+      },
+      '8': {
+        captureMark: false
+      },
+      '9': {
+        captureMark: false
+      },
+      '10': {
+        captureMark: false
+      },
+      '11': {
+        captureMark: false
+      },
+    },
+  },
+}
